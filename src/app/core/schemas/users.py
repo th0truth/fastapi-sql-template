919134dd008a8730ea99082bce327c6ee75bff91
th0truth <shiftrams@gmail.com>
2025-12-26 21:15:45 +0200

    feat(api): add user models

--- src/app/core/schemas/users.py
+++ src/app/core/schemas/users.py
@@ -1,10 +1,17 @@
 from sqlmodel import Field, SQLModel
 from pydantic import EmailStr
+from datetime import datetime
 from typing import Union
 
-class UserBase(SQLModel, table=True):
-  id: Union[int, None] = Field(default=None, primary_key=True)
+class UserBase(SQLModel):
   first_name: str
   middle_name: str
   last_name: str
   email: EmailStr
+  
+class User(UserBase, table=True):
+  id: Union[int, None] = Field(default=None, primary_key=True)
+
+class UserCreate(UserBase):
+  password: str
+  account_date: datetime
